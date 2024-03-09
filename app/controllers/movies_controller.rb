f70948e6950a24a7baed26964b5f1be8f14a95b8
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2024-03-09 18:00:56 +0200

    feat:add Sort by average stars

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,11 @@
 class MoviesController < ApplicationController
+  include MoviesHelper
   def index
-    @movies = Movie.all
+    @actor = params[:search] ? params[:search][:actor] : ''
+    @movies = Movie.left_outer_joins(:reviews)
+                   .select('movies.*, COALESCE(AVG(reviews.stars), 0) AS avg_rating')
+                   .where('actor LIKE ?', "%#{@actor}%")
+                   .group('movies.id')
+                   .order('avg_rating DESC')
   end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,15 @@
+# app/helpers/movies_helper.rb
+
 module MoviesHelper
+  def average_rating(movie)
+    reviews_count = movie.reviews.count
+
+    return 0 unless reviews_count > 0
+
+    total_rating = movie.reviews.sum(:stars).to_f
+    average_rating = total_rating / reviews_count
+    average_rating.round(2) # Round to two decimal places
+
+    # or any default value you prefer when there are no reviews
+  end
 end
