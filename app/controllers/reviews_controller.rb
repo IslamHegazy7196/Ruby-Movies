9b4716cc18e253c1eb55a8ac411e549bb9c8615a
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2024-03-09 19:03:50 +0200

    feat: add UI

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,13 @@
 class ReviewsController < ApplicationController
   def index
-    @reviews = Review.all
+    @movie_id = params[:movie_id]
+
+    @reviews = if @movie_id.present?
+                 Review.where(movie_id: @movie_id)
+               else
+                 Review.all
+               end
+
+    @movies = Movie.all
   end
 end
