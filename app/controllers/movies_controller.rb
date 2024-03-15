925b84336b84b41df388b2eabd1ac34655134324
IslamHegazy7196 <islamfathihegazy04@gmail.com>
2024-03-15 04:59:29 +0200

    fix: fix UI search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     @actor = params[:search] ? params[:search][:actor] : ''
     @movies = Movie.left_outer_joins(:reviews)
                    .select('movies.*, COALESCE(AVG(reviews.stars), 0) AS avg_rating')
-                   .where('actor LIKE ?', "%#{@actor}%")
+                   .joins(:actors).where('actors.name LIKE ?', "%#{@actor}%")
                    .group('movies.id')
                    .order('avg_rating DESC')
   end
